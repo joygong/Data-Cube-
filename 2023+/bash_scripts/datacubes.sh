fd554c1bd87550469a82b6e449a25dbc2249e557
Joy Gong <110737784+joygong@users.noreply.github.com>
2024-01-18 22:50:20 -0500

    Update datacubes.sh

--- 2023+/bash_scripts/datacubes.sh
+++ 2023+/bash_scripts/datacubes.sh
@@ -1,5 +1,4 @@
 
-#zs=$(for i in $(seq 1); do bc <<< "scale=3; ($RANDOM*0.7/32767)+0.3"; done)
 zs=$(for i in $(seq 1); do bc <<< "scale=3; ($RANDOM*1.7/32767)+0.3"; done)
 for z in $zs; do ./datacube.sh $z; done
 
